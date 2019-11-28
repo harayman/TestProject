22b5dc4c820c02c0bfaba29541d4e3f1ea83dbe3
Haray <harayman@163.com>
2019-11-28 19:28:50 +0700

    递交项目

---
+++ app/src/main/java/com/example/testprohect/CustomTest.java
@@ -0,0 +1,15 @@
+package com.example.testprohect;
+
+import com.example.mylibrary.TestShow;
+
+public class CustomTest {
+
+
+    public void test() {
+        TestShow testShow = new TestShow();
+        testShow.test();
+
+
+    }
+
+}
