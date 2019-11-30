70d3837c35ecebd1e43a84892b928552d92c7d7c
Haray <harayman@163.com>
2019-11-30 18:46:24 +0700

    添加渠道包

---
+++ app/src/main/java/com/example/testprohect/Main2Activity.java
@@ -0,0 +1,33 @@
+package com.example.testprohect;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Log.e("main","------"+getChannel());
+    }
+
+
+    /**
+     * 获取渠道名
+     */
+    private String getChannel() {
+        try {
+            PackageManager pm = getPackageManager();
+            ApplicationInfo appInfo = pm.getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+            return appInfo.metaData.getString("APP_CHANNEL");
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+        return "";
+    }
+
+}
